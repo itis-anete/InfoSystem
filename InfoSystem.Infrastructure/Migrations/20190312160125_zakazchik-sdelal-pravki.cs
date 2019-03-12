e0736637561321279ce0048050be2b5fc85e5998
d4n0n_myself <danon.sibaev@gmail.com>
2019-03-12 19:03:49 +0300

    Database migration to drop old tables, database not updated (will ruin master branch)

---
+++ InfoSystem.Infrastructure/Migrations/20190312160125_zakazchik-sdelal-pravki.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace InfoSystem.Infrastructure.Migrations
+{
+    public partial class zakazchiksdelalpravki : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attributes");
+
+            migrationBuilder.DropTable(
+                name: "Values");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attributes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true),
+                    TypeId = table.Column<int>(nullable: false),
+                    ValueType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attributes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    AttributeId = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    EntityId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attributes_Name",
+                table: "Attributes",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Values_AttributeId",
+                table: "Values",
+                column: "AttributeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Values_EntityId",
+                table: "Values",
+                column: "EntityId");
+        }
+    }
+}
