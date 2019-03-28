dc729e444dd78ff38859245c7fd96bc9f4a0043d
Helmoz <helmo.designer1@gmail.com>
2019-03-28 13:00:01 +0300

    Формы для регистрации и авторизации пользователя

---
+++ InfoSystem.Web/ClientApp/layouts/authentication.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app style="background-color: #F4F6F9">
+    <v-content>
+      <nuxt/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
